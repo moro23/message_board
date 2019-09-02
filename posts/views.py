c2d0c22ddb9dcc98047eacda3db4cc1929385767
Moro23 <billhab66@gmail.com>
2019-09-02 17:12:24 +0000

    added tests

---
+++ posts/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView
+from .models import Posts
+
+# Create your views here.
+class HomePageView(ListView):
+    model = Posts
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list' #new
